dc1d9dcbf79636fcc760f503554d97fc09f475b8
ashsajal1 <ashsajal1@gmail.com>
2024-01-24 17:20:17 +0800

    set start button with state

--- src/components/TypingTest.tsx
+++ src/components/TypingTest.tsx
@@ -24,6 +24,10 @@ export default function TypingTest() {
 
     const handleSubmit = () => {
         // console.log("submitted")
+        if(!isStarted) {
+            setIsStarted(true);
+            return
+        }
     }
 
     return (
@@ -31,9 +35,9 @@ export default function TypingTest() {
             <section className="p-2 flex flex-col gap-3">
                 <Timer time={timer} />
                 <p className="p-2 border runded text-lg font-medium">{text}</p>
-                <input onChange={handleInputChange} className="p-2 rounded border outline-none" title="Text field" type="text" />
+                <input disabled={!isStarted} onChange={handleInputChange} className="p-2 rounded border outline-none" title="Text field" type="text" />
 
-                <button onClick={handleSubmit} className="p-1 font-mono border rounded bg-teal-700 hover:bg-teal-800 text-slate-50">Submit</button>
+                <button onClick={handleSubmit} className="p-1 font-mono border rounded bg-teal-700 hover:bg-teal-800 text-slate-50">{ isStarted? "Submit":"Start" }</button>
             </section>
         </>
     )
