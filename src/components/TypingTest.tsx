de66f675c05a5e84d844cee95a8820c8d9f86f7c
ashsajal1 <ashsajal1@gmail.com>
2024-01-24 17:14:53 +0800

    add timer and text state

--- src/components/TypingTest.tsx
+++ src/components/TypingTest.tsx
@@ -1,16 +1,28 @@
-import { ChangeEvent, useState } from "react"
+import { ChangeEvent, useEffect, useState } from "react"
 import Timer from "./Timer";
 
 export default function TypingTest() {
+    const [text, setText] = useState('This is the text to write and test.');
     const [userInput, setUserInput] = useState('');
-    const [timer, setTimer] = useState(0);
+    const [timer, setTimer] = useState<number>(0);
+    const [isStarted, setIsStarted] = useState(false);
+
+    useEffect(() => {
+        if (isStarted) {
+          const interval = setInterval(() => {
+            setTimer((prevTimer) => prevTimer + 1);
+          }, 1000);
+    
+          return () => clearInterval(interval);
+        }
+      }, [isStarted]);
 
     const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
         setUserInput(e.target.value);
         // console.log(e.target.value);
     }
 
-    const handleSubmit= () => {
+    const handleSubmit = () => {
         // console.log("submitted")
     }
 
@@ -18,7 +30,7 @@ export default function TypingTest() {
         <>
             <section className="p-2 flex flex-col gap-3">
                 <Timer time={timer} />
-                <p className="p-2 border runded text-lg font-medium">This is the text to write and test.</p>
+                <p className="p-2 border runded text-lg font-medium">{text}</p>
                 <input onChange={handleInputChange} className="p-2 rounded border outline-none" title="Text field" type="text" />
 
                 <button onClick={handleSubmit} className="p-1 font-mono border rounded bg-teal-700 hover:bg-teal-800 text-slate-50">Submit</button>
