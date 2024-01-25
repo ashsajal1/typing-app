2c45cb01ff09c8f46a8c1f8ac4c07cd51f86a3f7
ashsajal1 <ashsajal1@gmail.com>
2024-01-25 14:13:30 +0800

    design result

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,8 +1,10 @@
 export default function Result({ accuracy, wpm }: { accuracy: number, wpm: number }) {
     return (
-        <section className="p-2 flex flex-col gap-3">
-            <div>Accuracy is {accuracy}%</div>
-            <div>WPM (Wrod Per Minute) : {wpm}</div>
-        </section>
+        <div className="h-screen flex items-center justify-center">
+            <section className="p-2 w-1/2 h-1/2 rounded flex flex-col gap-3 bg-teal-700 justify-center items-center text-slate-50">
+                <div className="">Accuracy : {accuracy}%</div>
+                <div>WPM (Word Per Minute) : {wpm}</div>
+            </section>
+        </div>
     )
 }
