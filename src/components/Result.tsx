2bc3dcbfe1cca65005ba014e6ef6f0d505839d77
ashsajal1 <ashsajal1@gmail.com>
2024-01-25 14:16:40 +0800

    Revert "test"
    This reverts commit 25103dc74424d94ace66a48719010adacc2fb3d0.

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -3,7 +3,7 @@ export default function Result({ accuracy, wpm }: { accuracy: number, wpm: numbe
         <div className="h-screen flex items-center justify-center">
             <section className="p-2 w-1/2 h-1/2 rounded flex flex-col gap-3 bg-teal-700 justify-center items-center text-slate-50">
                 <div className="">Accuracy : {accuracy}%</div>
-                <div>WPM (Word Per Minute) a: {wpm}</div>
+                <div>WPM (Word Per Minute) : {wpm}</div>
             </section>
         </div>
     )
