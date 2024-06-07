ad476ae79e101df3f89192e200a33b3287bedf11
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-07 12:35:01 +0600

    fix: zustand store type

--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,21 @@
 import create from 'zustand';
 import { persist } from 'zustand/middleware';
 
+interface StoreState {
+    isDarkMode: boolean;
+    toggleDarkMode: () => void;
+}
+
 const useStore = create(
-  persist(
-    (set) => ({
-      isDarkMode: false,
-      toggleDarkMode: () => set((state) => ({ isDarkMode: !state.isDarkMode })),
-    }),
-    {
-      name: 'theme', // name of item in storage
-    }
-  )
+    persist<StoreState>(
+        (set) => ({
+            isDarkMode: false,
+            toggleDarkMode: () => set((state) => ({ isDarkMode: !state.isDarkMode })),
+        }),
+        {
+            name: 'theme',
+        }
+    )
 );
 
 export default useStore;
