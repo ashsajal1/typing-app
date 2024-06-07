56838b7abbb8409d94927741bfeb5f066ad6595c
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-07 16:40:24 +0600

    add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,32 @@
+import { TypeIcon } from "lucide-react"
+
+export default function Footer() {
+    return (
+        <footer className="footer p-10 bg-base-200 text-base-content">
+            <aside>
+                <TypeIcon />
+                <p>Typing Practice App.<br />Practice typing</p>
+            </aside>
+            <nav>
+                <h6 className="footer-title">Services</h6>
+                <a className="link link-hover">Branding</a>
+                <a className="link link-hover">Design</a>
+                <a className="link link-hover">Marketing</a>
+                <a className="link link-hover">Advertisement</a>
+            </nav>
+            <nav>
+                <h6 className="footer-title">Company</h6>
+                <a className="link link-hover">About us</a>
+                <a className="link link-hover">Contact</a>
+                <a className="link link-hover">Jobs</a>
+                <a className="link link-hover">Press kit</a>
+            </nav>
+            <nav>
+                <h6 className="footer-title">Legal</h6>
+                <a className="link link-hover">Terms of use</a>
+                <a className="link link-hover">Privacy policy</a>
+                <a className="link link-hover">Cookie policy</a>
+            </nav>
+        </footer>
+    )
+}
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,12 +1,14 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 // import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 export const Route = createRootRoute({
   component: () => (
     <>
       <Navbar />
       <Outlet />
+      <Footer />
       {/* <TanStackRouterDevtools /> */}
     </>
   ),
