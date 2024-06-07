9735c21b9f93da0429b499010f060d89db1a50ae
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-07 22:25:58 +0600

    refactor: map topics array

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,29 @@
 import { ChangeEvent, useState } from "react";
-import TypingTest from "./components/TypingTest";
-import generateRandomWords from "./lib/generateRandomWords";
+import { useSentenceStore } from "./store/sentenceStore";
 
 export default function App() {
   const [selectedTopics, setSelectedTopics] = useState('')
 
-  const handleSelectChange = (event:ChangeEvent<HTMLSelectElement>) => {
+  const handleSelectChange = (event: ChangeEvent<HTMLSelectElement>) => {
     const value = event.target.value;
     setSelectedTopics(value);
   };
+
+  const { getAllTopics } = useSentenceStore();
+  const topics = getAllTopics();
 
   return (
     <>
       <div className="flex flex-col gap-3 p-4">
         <select value={selectedTopics} onChange={handleSelectChange} className="select select-success w-full">
           <option disabled selected>Pick your favorite anime</option>
-          <option>One Piece</option>
-          <option>Naruto</option>
-          <option>Death Note</option>
-          <option>Attack on Titan</option>
-          <option>Bleach</option>
-          <option>Fullmetal Alchemist</option>
-          <option>Jojo's Bizarre Adventure</option>
+          {topics.map(topic => (
+            <option>{topic}</option>
+          ))}
         </select>
 
         <button className="btn btn-active btn-success">Start Practice</button>
       </div>
-      {/* <TypingTest text={generateRandomWords()} /> */}
     </>
   )
 }
