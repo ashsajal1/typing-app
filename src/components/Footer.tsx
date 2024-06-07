983235d573d532305dc82d7f9a91c6c21d0fae74
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-07 18:25:41 +0600

    feat: add copyright footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,31 +2,39 @@ import { TypeIcon } from "lucide-react"
 
 export default function Footer() {
     return (
-        <footer className="footer p-10 bg-base-200 dark:bg-base-300 text-base-content">
-            <aside>
-                <TypeIcon />
-                <p>Typing Practice App.<br />Practice typing</p>
-            </aside>
-            <nav>
-                <h6 className="footer-title">Services</h6>
-                <a className="link link-hover">Branding</a>
-                <a className="link link-hover">Design</a>
-                <a className="link link-hover">Marketing</a>
-                <a className="link link-hover">Advertisement</a>
-            </nav>
-            <nav>
-                <h6 className="footer-title">Company</h6>
-                <a className="link link-hover">About us</a>
-                <a className="link link-hover">Contact</a>
-                <a className="link link-hover">Jobs</a>
-                <a className="link link-hover">Press kit</a>
-            </nav>
-            <nav>
-                <h6 className="footer-title">Legal</h6>
-                <a className="link link-hover">Terms of use</a>
-                <a className="link link-hover">Privacy policy</a>
-                <a className="link link-hover">Cookie policy</a>
-            </nav>
-        </footer>
+        <>
+            <footer className="footer p-10 bg-base-200 dark:bg-base-300 text-base-content">
+                <aside>
+                    <TypeIcon />
+                    <p>Typing Practice App.<br />Practice typing</p>
+                </aside>
+                <nav>
+                    <h6 className="footer-title">Services</h6>
+                    <a className="link link-hover">Branding</a>
+                    <a className="link link-hover">Design</a>
+                    <a className="link link-hover">Marketing</a>
+                    <a className="link link-hover">Advertisement</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Company</h6>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Contact</a>
+                    <a className="link link-hover">Jobs</a>
+                    <a className="link link-hover">Press kit</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Legal</h6>
+                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+                    <a className="link link-hover">Cookie policy</a>
+                </nav>
+            </footer>
+
+            <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+                <aside>
+                    <p>Copyright &copy; {new Date().getFullYear()} - All right reserved by Typing Practice App</p>
+                </aside>
+            </footer>
+        </>
     )
 }
