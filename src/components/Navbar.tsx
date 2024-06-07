c5caa81bcd22896acba2b7d3da5f61180532a5b9
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-07 15:47:51 +0600

    feat: remove link form category btn

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,9 +7,7 @@ export default function Navbar() {
       <Link to='/' className="text-success">Typing practice app</Link>
       <div className="flex items-center gap-2">
         <DarkModeToggle />
-        <Link to="/category">
-          <button className="btn btm-nav-sm btn-success">Category</button>
-        </Link>
+        <button className="btn btm-nav-sm btn-success">Category</button>
       </div>
     </nav>
   )
