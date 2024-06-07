0eff792484e405cad426f5704d1549397daff396
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-07 14:29:15 +0600

    style: add border

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import DarkModeToggle from "./DarkModeToggle";
 
 export default function Navbar() {
   return (
-    <nav className="flex items-center justify-between p-4 shadow dark:bg-black">
+    <nav className="flex items-center dark:border-b dark:border-b-gray-700 justify-between p-4 shadow dark:bg-black">
         <div className="text-success">Typing practice app</div>
         <div className="flex items-center gap-2">
             <DarkModeToggle />
