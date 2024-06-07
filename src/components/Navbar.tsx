c268578498373004ca76905862f5b8613973a2da
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-07 12:19:54 +0600

    feat: add icon in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,13 @@
+import { SunIcon } from "lucide-react";
+
 export default function Navbar() {
   return (
     <nav className="flex items-center justify-between p-4 shadow">
         <div>Typing practice app</div>
-        <div>
+        <div className="flex items-center gap-2">
+            <button className="btn">
+                <SunIcon />
+            </button>
             <button className="btn btm-nav-sm btn-success">Category</button>
         </div>
     </nav>
