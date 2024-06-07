b8a5da2ea788cf1e609318fbe51f66f34a8f45f4
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-07 22:21:29 +0600

    feat: add select for topic

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
+import { ChangeEvent, useState } from "react";
 import TypingTest from "./components/TypingTest";
 import generateRandomWords from "./lib/generateRandomWords";
 
 export default function App() {
+  const [selectedTopics, setSelectedTopics] = useState('')
+
+  const handleSelectChange = (event:ChangeEvent<HTMLSelectElement>) => {
+    const value = event.target.value;
+    setSelectedTopics(value);
+  };
+
   return (
     <>
-    <TypingTest text={generateRandomWords()} />
+      <div className="flex flex-col gap-3 p-4">
+        <select value={selectedTopics} onChange={handleSelectChange} className="select select-success w-full">
+          <option disabled selected>Pick your favorite anime</option>
+          <option>One Piece</option>
+          <option>Naruto</option>
+          <option>Death Note</option>
+          <option>Attack on Titan</option>
+          <option>Bleach</option>
+          <option>Fullmetal Alchemist</option>
+          <option>Jojo's Bizarre Adventure</option>
+        </select>
+
+        <button className="btn btn-active btn-success">Start Practice</button>
+      </div>
+      {/* <TypingTest text={generateRandomWords()} /> */}
     </>
   )
 }
