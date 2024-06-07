2066c76b3c92b2f4d6c548aaf89311b7d7c63a38
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-07 23:42:20 +0600

    feat: add btn for back and home

--- src/routes/practice.tsx
+++ src/routes/practice.tsx
@@ -1,8 +1,9 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { Link, createFileRoute } from '@tanstack/react-router'
 import { z } from 'zod'
 import { useSentenceStore } from '../store/sentenceStore'
 import TypingTest from '../components/TypingTest'
 import { shuffleArray } from '../lib/utils'
+import { ArrowLeft, HomeIcon } from 'lucide-react'
 
 const topicsSearchSchema = z.object({
   topic: z.string().catch('biology'),
@@ -17,8 +18,23 @@ const Practice = () => {
   const { topic, eclipsedTime } = Route.useSearch()
   const sentences = useSentenceStore((state) => state.getSentencesByTopic(topic || ''));
   if (sentences.length === 0) {
-    return <div>
-      Not found text of topic {topic}
+    return <div className='grid place-items-center p-12'>
+      <div className='flex flex-col items-center'>
+        <h1 className='mb-4 font-bold text-3xl'>Not found text of topic {topic}!</h1>
+
+        <div className='flex items-center gap-2'>
+          <button onClick={() => history.back()} className='btn btn-success btn-outline'>
+            <ArrowLeft className='h-5 w-5' />
+            Back
+          </button>
+          <Link to='/'>
+            <button className='btn btn-success btn-outline'>
+              <HomeIcon className='h-5 w-5' />
+              Go Home
+            </button>
+          </Link>
+        </div>
+      </div>
     </div>
   }
   return <TypingTest eclipsedTime={eclipsedTime} text={shuffleArray([...sentences]).join(" ").slice(0, 500)} />
