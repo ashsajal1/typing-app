53daf4f243a5401d8283862ff51625a25f6c2c81
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-07 12:15:48 +0600

    feat: create navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  return (
+    <nav className="flex items-center justify-between p-4 shadow">
+        <div>Typing practice app</div>
+        <div>
+            <button className="btn btm-nav-sm btn-success">Category</button>
+        </div>
+    </nav>
+  )
+}
