21fd71d25886aaa651242b3edf1c51c24ab9a575
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-07 21:37:08 +0600

    feat: handle not found topic

--- src/routes/practice.tsx
+++ src/routes/practice.tsx
@@ -18,5 +18,10 @@ const Practice = () => {
   const { topic } = Route.useSearch()
   const sentences = useSentenceStore((state) => state.getSentencesByTopic(topic || ''));
   // console.log(sentences.sort(() => Math.floor(Math.random() * 3) - 1))
+  if(sentences.length === 0) {
+    return <div>
+      Not found text of topic {topic}
+    </div>
+  }
   return <TypingTest text={sentences.sort(() => Math.floor(Math.random() * 3) - 1).join(" ").slice(0, 500)} />
 }
