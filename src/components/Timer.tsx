ff2ee95f31ba70014c70fc00711f419093af30bd
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-06-07 15:55:00 +0600

    style: change color to success

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -2,7 +2,7 @@ import { ClockIcon } from "lucide-react"
 
 export default function Timer({ time }: { time: number }) {
     return (
-        <div className="rounded border border-teal-700 p-2 flex items-center gap-2 justify-center text-teal-700">
+        <div className="rounded border border-success p-2 flex items-center gap-2 justify-center text-success">
             <ClockIcon className="h-4 w-4" />
             <span>{time}</span>
         </div>
