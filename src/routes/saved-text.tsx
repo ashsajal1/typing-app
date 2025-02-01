ff6de3a7215ac8ea650bd06f55394894b8f523f1
Ashsajal <ashsajal1@gmail.com>
2025-02-01 21:46:40 +0600

    style: wfull

--- src/routes/saved-text.tsx
+++ src/routes/saved-text.tsx
@@ -30,23 +30,30 @@ function RouteComponent() {
       <div className="grid gird-cols-1 md:grid-cols-2 gap-4 w-full">
         {existingData.map((data, index) => (
           <div
-            className="flex flex-col justify-between items-center w-full shadow p-2 border border-base-200 rounded"
+            className="flex flex-col justify-between items-center w-full shadow p-3 gap-2 border border-base-200 rounded"
             key={index}
           >
-            <div className="card-body">
+            <div>
               <div>
                 <p className="card-title">{data.label.slice(0, 50)}...</p>
-                <div className="flex o items-center gap-2">
-                  
-                  <button className="btn btn-error btn-sm"><Trash2 className="w-4 h-4" /></button>
-                  <button className="btn btn-sm"><Pencil className="w-4 h-4" /></button>
+                <div className="flex items-center gap-2">
+                  <button className="btn btn-error btn-sm">
+                    <Trash2 className="w-4 h-4" />
+                  </button>
+                  <button className="btn btn-sm">
+                    <Pencil className="w-4 h-4" />
+                  </button>
                 </div>
               </div>
               <p className="font-light text-sm">{data.text.slice(0, 150)}...</p>
             </div>
             <div className="flex w-full items-center justify-between gap-2">
-              <Link to="/practice" search={{ savedTextId: data.id }}>
-                <button className="btn w-full">Practice</button>
+              <Link
+                to="/practice"
+                className="w-full btn"
+                search={{ savedTextId: data.id }}
+              >
+                Practice
               </Link>
             </div>
           </div>
