e274eb0b0016496acf7ee3e06112bab1ebe2322d
Ashsajal <ashsajal1@gmail.com>
2025-02-01 21:39:48 +0600

    feat: add saved link to nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,17 @@
 import { Link } from "@tanstack/react-router";
 import ModeToggle from "./ModeToggle";
+import { Bookmark } from "lucide-react";
 
 export default function Navbar() {
   return (
     <nav className="flex items-center bg-base-100 dark:bg-base-300 dark:border-b dark:border-b-gray-700 justify-between p-4 shadow">
-      <Link to='/' className="text-success">Typing practice app</Link>
+      <Link to="/" className="text-success">
+        Typing practice app
+      </Link>
       <div className="flex items-center gap-2">
+        <Link to="/saved-text" className="btn btn-outlined"><Bookmark size={20} strokeWidth={1.3} /> Saved Text</Link>
         <ModeToggle />
       </div>
     </nav>
-  )
+  );
 }
