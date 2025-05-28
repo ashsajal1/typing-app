826c712855d1f053ba7ce255750765a0981e0359
Ashsajal <ashsajal1@gmail.com>
2025-05-28 19:50:06 +0600

    feat: setup cypress

---
+++ cypress.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'cypress'
+import codeCoverageTask from '@cypress/code-coverage/task'
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:5173',
+    supportFile: 'cypress/support/e2e.ts',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    video: false,
+    screenshotOnRunFailure: true,
+    setupNodeEvents(on, config) {
+      codeCoverageTask(on, config)
+      return config
+    },
+  },
+  component: {
+    devServer: {
+      framework: 'react',
+      bundler: 'vite',
+    },
+    supportFile: 'cypress/support/component.ts',
+    specPattern: 'cypress/component/**/*.cy.{js,jsx,ts,tsx}',
+  },
+}) 
---
+++ cypress/support/e2e.ts
@@ -0,0 +1,5 @@
+import '@cypress/code-coverage/support'
+import './commands'
+
+// Alternatively you can use CommonJS syntax:
+// require('./commands') 
