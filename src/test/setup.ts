18faaa7b61aebba4be2fe2066a30b6ce265af2c3
Ashsajal <ashsajal1@gmail.com>
2025-05-28 19:23:51 +0600

    fix: update import for jest-dom matchers to use namespace import

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom';
 import { expect, afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
-import matchers from '@testing-library/jest-dom/matchers';
+import * as matchers from '@testing-library/jest-dom/matchers';
 
 // Extend Vitest's expect method with methods from react-testing-library
 expect.extend(matchers);
