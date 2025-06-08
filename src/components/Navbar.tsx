cc15160d2a84a015dfc5bfd2bc47b87a30aeca70
Ashsajal <ashsajal1@gmail.com>
2025-06-08 09:42:50 +0600

    feat: change logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,13 @@
 import { Link } from "@tanstack/react-router";
 import ModeToggle from "./ModeToggle";
-import { Bookmark, BarChart2 } from "lucide-react";
+import { Bookmark, BarChart2, Keyboard } from "lucide-react";
 
 export default function Navbar() {
   return (
     <nav className="flex items-center bg-base-100 dark:bg-base-300 dark:border-b dark:border-b-gray-700 justify-between p-4 shadow">
-      <Link to="/" className="text-success">
-        Typing practice app
+      <Link to="/" className="text-success flex items-center gap-2">
+        <Keyboard size={20} strokeWidth={1.3} />
+        <span className="font-medium">TypingPro</span>
       </Link>
       <div className="flex items-center gap-2">
         <Link to="/stats" className="btn btn-outlined">
