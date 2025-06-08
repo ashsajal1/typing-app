d2b8342aff4f20c56b983b1d4e5322a02149b8f9
Ashsajal <ashsajal1@gmail.com>
2025-06-08 13:15:35 +0600

    ✨ feat: wrap in HelmetProvider

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,15 +1,18 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
+import { HelmetProvider } from 'react-helmet-async'
 // import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 
 export const Route = createRootRoute({
   component: () => (
-    <div className="overflow-x-hidden">
-      <Navbar />
-      <Outlet />
-      <Footer />
-      {/* <TanStackRouterDevtools /> */}
-    </div>
+    <HelmetProvider>
+      <div className="overflow-x-hidden">
+        <Navbar />
+        <Outlet />
+        <Footer />
+        {/* <TanStackRouterDevtools /> */}
+      </div>
+    </HelmetProvider>
   ),
 })
