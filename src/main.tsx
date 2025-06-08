e6cad1f14bc408f09cb3c1e4281b717e022658cc
Ashsajal <ashsajal1@gmail.com>
2025-06-08 14:02:11 +0600

    ✨ feat: import i18n config

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
+import './i18n/i18n' // Import i18n configuration
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
